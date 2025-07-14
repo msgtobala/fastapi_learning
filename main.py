d46c60d15b4a117092b27370829e9935a1d4c156
Balaji Venkatraman <rslbalaji@gmail.com>
2025-07-14 20:45:29 +0530

    initial files

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def index() -> dict[str, str]:
+    return {"message": "Hello World"}
+
+@app.get("/about")
+async def about() -> str:
+    return "About page"
